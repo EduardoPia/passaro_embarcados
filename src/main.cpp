f78d5a4faa090e79e8e4ccbebe74e41929e29e2a
Eduardo Piassaroli de Abreu <146479077+EduardoPia@users.noreply.github.com>
2025-03-17 11:25:14 -0300

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 #include <Arduino.h>
-#include <LiquidCrystal.h>
+// #include <LiquidCrystal.h>
 
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+// const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+// LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-static const int POT_pin   = -1;
-static const int LDR_pin   = -1;
-static const int SERVO_pin = -1;
-static const int LED_pin   = -1;
+static const int POT_pin   = 4;
+static const int LDR_pin   = 18;
+static const int SERVO_pin = 27;
+static const int LED_pin   = 14;
 
 static const int res = 12;
 static const int freq = 50;
@@ -20,10 +20,10 @@ int LDR_value = 0;
 int period    = -1;
 int period_blink = -1;
 
-enum FLIGHT_MODE {Auto, Manual} mode;
+enum FLIGHT_MODE {Auto, Manual};
 FLIGHT_MODE mode = Manual; 
 
-enum DAY_TIME {Day, NightFall, Night} mode;
+enum DAY_TIME {Day, NightFall, Night};
 DAY_TIME day_time = Day; 
 
 void read_pot();
@@ -39,13 +39,13 @@ setup()
 {
 	analogReadResolution(res); // pot
 	pinMode(LDR_pin, INPUT_PULLUP); // ldr
-	pinMode(LED_pin,OUTPUT);
+	pinMode(LED_pin, OUTPUT);
 
 	ledcSetup(channel, freq, res); // servo
 	ledcAttachPin(SERVO_pin, channel);
 
-	lcd.begin(16, 2);
-	lcd.print("Time to FLY!");
+	// lcd.begin(16, 2);
+	// lcd.print("Time to FLY!");
 
 	Serial.begin(9600);
 }
@@ -99,12 +99,13 @@ update_servo()
 void 
 update_eyes()
 {
-	double time = millis() % period_blink;
-	if(time < (0.8*period_blink)) {
-		digitalWrite(LED_pin,HIGH);
-	} else {
-		digitalWrite(LED_pin,LOW);
-	} 
+	digitalWrite(LED_pin, HIGH);
+	// double time = millis() % period_blink;
+	// if(time < (0.8*period_blink)) {
+	// 	digitalWrite(LED_pin,HIGH);
+	// } else {
+	// 	digitalWrite(LED_pin,LOW);
+	// } 
 
 }
 
@@ -112,25 +113,25 @@ update_eyes()
 void 
 update_LCD()
 {
-	if (day_time == Day) {
-		lcd.setCursor(0,0);
-		lcd.print("Dia: Voando");
+	// if (day_time == Day) {
+	// 	lcd.setCursor(0,0);
+	// 	lcd.print("Dia: Voando");
 
-	} else if (day_time == NightFall) {
-		lcd.setCursor(0,0);
-		lcd.print("Noite: Fugindo");
-	} else {
-		lcd.setCursor(0,0);
-		lcd.print("Noite: Dormindo");
-	}
+	// } else if (day_time == NightFall) {
+	// 	lcd.setCursor(0,0);
+	// 	lcd.print("Noite: Fugindo");
+	// } else {
+	// 	lcd.setCursor(0,0);
+	// 	lcd.print("Noite: Dormindo");
+	// }
 
-	if(mode == Manual) {
-		lcd.setCursor(0,1);
-		lcd.print("Modo: Manual");
-	} else {
-		lcd.setCursor(0,1);
-		lcd.print("Modo: Auto");
-	}
+	// if(mode == Manual) {
+	// 	lcd.setCursor(0,1);
+	// 	lcd.print("Modo: Manual");
+	// } else {
+	// 	lcd.setCursor(0,1);
+	// 	lcd.print("Modo: Auto");
+	// }
 }
 
 void
